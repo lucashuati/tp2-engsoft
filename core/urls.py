6d50553ce64ffcd2429d77dd7fd26532243ea6e3
Nícolas Maduro <nicolasamaduro@gmail.com>
2017-06-13 01:26:14 -0300

    cdu - lista de cadernos

--- core/urls.py
+++ core/urls.py
@@ -16,4 +16,11 @@ urlpatterns = [
     url(r'^editar/caderno/(?P<pk>\d+)$', editar_caderno.as_view(), name = 'editarcaderno'),
     url(r'^cadernos/(?P<pk>\d+)$', mostrar_caderno.as_view(), name='mostrarcaderno'),
 
+    url(r'^new/listaCaderno/$', criarListaCaderno.as_view(), name='criarListaCaderno'),
+    url(r'^listaCadernos/$', lista_lista_cadernos, name='listaCadernos'),
+    url(r'^excluir/listaCaderno/$', excluir_lista_cadernos, name='excluirListaCadernos'),
+    url(r'^editar/listaCaderno/$', editar_lista_cadernos, name='editarListaCadernos'),
+    url(r'^excluir/listaCaderno/(?P<pk>\d+)$', deletar_lista_caderno.as_view(), name='deletarListaCaderno'),
+    url(r'^editar/listaCaderno/(?P<pk>\d+)$', editar_lista_caderno.as_view(), name = 'editarListaCaderno'),
+    url(r'^listaCadernos/(?P<pk>\d+)$', mostrar_lista_caderno.as_view(), name='mostrarListaCaderno'),
 ]
